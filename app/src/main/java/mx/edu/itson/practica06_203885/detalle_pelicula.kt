12a42b5a7b7e320f95ddce2817db54d54d17d8bf
RCardoza00 <71920464+RCardoza00@users.noreply.github.com>
2021-03-08 00:34:11 -0700

    fixed bug

--- app/src/main/java/mx/edu/itson/practica06_203885/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/itson/practica06_203885/detalle_pelicula.kt
@@ -7,9 +7,11 @@ class detalle_pelicula : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pelicula)
 
-        var bundle= intent.extras
+        var bundle=intent.extras
         if(bundle!=null){
-
+            iv_pelicula_img.setImageResource(bundle.getInt("header"))
+            tv_nombre_pelicula.setText(bundle.getString("titulo"))
+            tv_description.setText(bundle.getString("sinopsis"))
         }
     }
 }
